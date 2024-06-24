0d1714c7d61a61c7240818d9aca23ccc099d4e7f
Lewis Freeman <lewis_3man@hotmail.com>
2024-06-24 21:02:20 +0000

    Added code for multiply game

--- assets/java/script.js
+++ assets/java/script.js
@@ -30,8 +30,9 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2)
-    }
-    else {
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2)
+    } else {
         alert(`Unknown game type: ${gameType}`)
         throw `Unknown game type: ${gameType}. Aborting!`
     }
@@ -69,7 +70,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+")
         return [operand1 + operand2, "addition"]
-    else{
+    else if(operator === "x") {
+        return [operand1 * operand2, "multiply"]
+    } else{
         alert(`Unimplemented operator ${operator}`)
         throw (`unimplemented operator ${operator}. Aborting!`)
     }
@@ -107,11 +110,17 @@ function displayAdditionQuestion(operand1, operand2) {
 }
 
 
+
 function displaySubtractQuestion() {
-    
+
 }
 
-function displayMultiplyQuestion() {
+
+function displayMultiplyQuestion(operand1, operand2) {
     
+    document.getElementById('operand1').textContent = operand1
+    document.getElementById('operand2').textContent = operand2
+    document.getElementById('operator').textContent = 'x'
+
 }
 
