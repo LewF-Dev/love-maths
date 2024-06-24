48dd69525f5d9e244aa4738ede20152edbc6699d
Lewis Freeman <lewis_3man@hotmail.com>
2024-06-24 19:40:04 +0000

    Added code to keep count of the users score and incorrect answers

--- assets/java/script.js
+++ assets/java/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
     
     if (isCorrect) {
         alert("Hey! You got it right! :D")
+        incrementScore()
     } else {
         alert(`Awww, you answered ${userAnswer}. The correct answer was: ${calculatedAnswer[0]}!`)
+        incrementWrongAnswer()
     }
     runGame(calculatedAnswer[1])
 }
@@ -66,7 +68,7 @@ function calculateCorrectAnswer() {
     let operator = document.getElementById("operator").innerText
 
     if (operator === "+")
-        return [operand1 +operand2, "addition"]
+        return [operand1 + operand2, "addition"]
     else{
         alert(`Unimplemented operator ${operator}`)
         throw (`unimplemented operator ${operator}. Aborting!`)
@@ -75,14 +77,27 @@ function calculateCorrectAnswer() {
     runGame(calculatedAnswer)
 
 }
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
 function incrementScore() {
     
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore
 }
 
+/**
+ * Gets the current number of incorrect answers from the DOM and increments it by 1
+ */
+
 function incrementWrongAnswer() {
-    
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore
 }
+        
+
 
 function displayAdditionQuestion(operand1, operand2) {
 
